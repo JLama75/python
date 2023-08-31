fab65df145d4461eb71fb4e429f0639d811208e4
JLama75 <97466232+JLama75@users.noreply.github.com>
2023-08-30 22:09:53 -0400

    Update Boyer-Moore_Algorithm.py

--- Boyer-Moore_Algorithm.py
+++ Boyer-Moore_Algorithm.py
@@ -1,51 +1,41 @@
-#Boyer-Moore:
-#skipping some uneccessary alignment
+#Boyer-Moore: Does alignment while skipping some unnecessary alignments
+#           : Does alignments in left-to-right order, and character comparisons in right-to-left order
 
-#We will try alignments in left-to-right order, and try
-#character comparisons in right-to-left order
-#P:word
-#T:There would have been a time for such a word
-#   -------word---------------------------->
-#           <--
-
-#Bad character Rule:
+#Rules:
+#1) Bad character Rule:
 #Upon mismatch, skip alignments until (a) mismatch becomes a match, or (b) P moves past mismatched character
-
-#Good suffix rule: Let t=substring matched by inner loop: skip until (a) there are no mismatches between P and t or (b)
-#P moves past t
-
+#2) Good suffix rule: Let t=substring matched by inner loop: skip until (a) there are no mismatches between P and t or (b)P moves past t
 #So, using those rules it skips alignments and is faster than naive exact matching
 
-#Boyer-Moore:Prerocessing
-#Pre-calculate skips. For bad character rule, P =TCGC: look up tables
+#Processing steps: Pre-calculates skips. For example, you can calculate the total number of shifts/skips using the two rules for the pattern(P) = TCGC. It makes a look up tables with shift values
 
-#Creating a boyer_moore function that takes a pattern, text and preprocessed boyerMoore object
-#This function does the alignment and character matching by skipping/shifting past unncessarry alignments using info provided by boyerMoore object
+#Now let's create a boyer_moore function that takes a pattern, text, and preprocessed boyerMoore object
+#This function does the alignment and character matching by skipping/shifting past unnecessary alignments using the info provided by the boyerMoore object
 
 def boyer_moore(p, p_bm, t):
     i = 0
     occurrences = [] #list to store indices that give matches
     while i < len(t) - len(p) + 1:
         shift = 1 #default shift is 1
-        mismatched = False #initialize the boolean variable 'mismatch' to false
-        for j in range(len(p) -1, -1, -1):#inner loop goes from len(p)-1 backwords until just before -1
+        mismatched = False # Initialize the boolean variable 'mismatch' to false
+        for j in range(len(p) -1, -1, -1):#inner loop goes from len(p)-1 backward until just before -1
             if not p[j] == t[i+j]: #incase there is a mismatch
                 #Calculate how many alignments to shift. Using the preprocessing function
                 skip_bc = p_bm.bad_character_rule(j, t[i+j]) #object.functionWithinClass(parameters)
-                skip_gs = p_bm.good_suffix_rule(j, t[i+j]) #parameters= offset where mismatch occurs, mistmatching text 't'
+                skip_gs = p_bm.good_suffix_rule(j, t[i+j]) #parameters= offset where mismatch occurs, mismatching text 't'
                 shift = max(shift, skip_bc, skip_gs) #whichever is the largest will save us the most time
                 mismatched = True #set mismatch to True and break
                 break
         if not mismatched: #If there was no mismatch
-            occurrences.append(i) #gives the offset/postition where the match occured
-            skip_gs = p_bm.match_skip() #still needs to shift using good suffix (gs) rule
+            occurrences.append(i) #gives the offset/position where the match occurred
+            skip_gs = p_bm.match_skip() #still needs to shift using the good suffix (gs) rule
             shift = max(shift, skip_gs)
         i += shift #make the shift
     return occurrences
 
 t = 'GCTACGATCTAGAATCTA'
 p = 'TCTA'
-#creating BoyerMoore object that will preprocess the pattern p and gives you how many indices to skip
+# Creating a BoyerMoore object that will preprocess the pattern p and give you how many indices to skip
 p_bm = BoyerMoore(p) #calling function
 boyer_moore(p, p_bm, t)
 
