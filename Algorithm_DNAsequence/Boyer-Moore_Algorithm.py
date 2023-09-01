d13422e5b89a6fe8835d10c45025481ec590eb51
Jyoti Lama <97466232+JLama75@users.noreply.github.com>
2023-09-01 16:17:02 -0400

    Update Boyer-Moore_Algorithm.py

--- Algorithm_DNAsequence/Boyer-Moore_Algorithm.py
+++ Algorithm_DNAsequence/Boyer-Moore_Algorithm.py
@@ -5,9 +5,9 @@
 #1) Bad character Rule:
 #Upon mismatch, skip alignments until (a) mismatch becomes a match, or (b) P moves past mismatched character
 #2) Good suffix rule: Let t=substring matched by inner loop: skip until (a) there are no mismatches between P and t or (b)P moves past t
-#So, using those rules it skips alignments and is faster than naive exact matching
+#So, using these two rules it skips alignments and is faster than naive exact matching. Which rule to use depends on which rule gives maximum number of skips.
 
-#Processing steps: Pre-calculates skips. For example, you can calculate the total number of shifts/skips using the two rules for the pattern(P) = TCGC. It makes a look up tables with shift values
+#Processing steps: Pre-calculates skips. For example, you can calculate the total number of shifts/skips using the two rules for the pattern(P) = TCGC. It makes a look-up table with the shift values
 
 #Now let's create a boyer_moore function that takes a pattern, text, and preprocessed boyerMoore object
 #This function does the alignment and character matching by skipping/shifting past unnecessary alignments using the info provided by the boyerMoore object
@@ -20,7 +20,7 @@ def boyer_moore(p, p_bm, t):
         mismatched = False # Initialize the boolean variable 'mismatch' to false
         for j in range(len(p) -1, -1, -1):#inner loop goes from len(p)-1 backward until just before -1
             if not p[j] == t[i+j]: #incase there is a mismatch
-                #Calculate how many alignments to shift. Using the preprocessing function
+                #Calculate how many alignments to shift by calling the preprocessing function, bad character rule, and good suffix rule
                 skip_bc = p_bm.bad_character_rule(j, t[i+j]) #object.functionWithinClass(parameters)
                 skip_gs = p_bm.good_suffix_rule(j, t[i+j]) #parameters= offset where mismatch occurs, mismatching text 't'
                 shift = max(shift, skip_bc, skip_gs) #whichever is the largest will save us the most time
@@ -30,13 +30,13 @@ def boyer_moore(p, p_bm, t):
             occurrences.append(i) #gives the offset/position where the match occurred
             skip_gs = p_bm.match_skip() #still needs to shift using the good suffix (gs) rule
             shift = max(shift, skip_gs)
-        i += shift #make the shift
+        i += shift #make the shift and skips unnecessary alignments
     return occurrences
 
 t = 'GCTACGATCTAGAATCTA'
 p = 'TCTA'
 # Creating a BoyerMoore object that will preprocess the pattern p and give you how many indices to skip
-p_bm = BoyerMoore(p) #calling function
+p_bm = BoyerMoore(p) #creating p_bm object by calling the BoyerMoore class
 boyer_moore(p, p_bm, t)
 
 #double check the offset
