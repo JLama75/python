b704fe4ce7b9211d5c8bc39475428b7096c5aac6
Jyoti Lama <97466232+JLama75@users.noreply.github.com>
2023-09-01 15:51:53 -0400

    Update GC_content.py

--- Algorithm_DNAsequence/GC_content.py
+++ Algorithm_DNAsequence/GC_content.py
@@ -1,4 +1,4 @@
-#Lets read a fastq file, store the sequences and base quality info of the reads into a list
+# Let's read a fastq file, store the sequences, and base quality info of the reads into a list
 #Convert the ASCII value of base quality into an integer score and create a histogram
 #
 def readFastq(filename):
@@ -10,7 +10,7 @@ def readFastq(filename):
             seq = fh.readline().rstrip()
             fh.readline()
             qual = fh.readline().rstrip()
-            if len(seq) == 0:
+            if len(seq) == 0: #after reaching the end of the file
                 break
             sequences.append(seq)
             qualities.append(seq)
@@ -22,19 +22,20 @@ print(seqs[:5])
 print(quals[:5])
 
 # Let's first work with the base qualities 
+#This function converts phred33 value to quality scores
 def phred33toQ(quals):
     return ord(quals) - 33
-
-phred33toQ('#')
+    
 #2 --> low quality score.
-phred33toQ('J')
-#41
+#phred33toQ('J') --> 41
+
+#Now create a histogram of quality scores
 def createHist(qualities):
-    hist = [0] * 60 #length of the hist list is set to 60
+    hist = [0] * 60 #length of the hist list is set to 60, will the x-axis
     for qual in qualities: # one string of qualities in a list of qualities
         for phred in qual: # each base in each qual string
-            q = phred33toQ(phred) # convert character to quality score
-            hist[q] += 1 # Count the number of times with that quality score and store the count in 'q' position of list 'hist'
+            q = phred33toQ(phred) 
+            hist[q] += 1 # Count the number of times with that quality score and store the count in the 'qth' position of the list 'hist'. The count will be the y-axis
     return hist
 h = createHist(quals)
 print(h)
@@ -46,17 +47,15 @@ plt.show()
 
 #GC contents
 def findGCByPos(reads):
-    gc = [0] * 100 #length of each sequence is 100. So putting gc counts in 100 positions (x-axis)
+    gc = [0] * 100 #length of read sequence(100). So plotting GC counts in each of the 100 base positions (x-axis=base position of reads)
     totals =  [0] * 100 # total number of bases at each position #should be equal to total number of reads
-
-    for read in reads: #looping through each sequence in list reads. reads is a list of 1000s of sequences
+    for read in reads: #looping through each sequence in list reads (millions). 
         for i in range(len(read)): #looping through each base position 'i' in the selected sequence read
             if read[i] == 'C' or read[i] == 'G': #seeing if that base position 'i' has C or G
-                gc[i]  += 1                      #counting incase it is a G or C
-            totals[i] += 1 #regardless what that base is, counting the total bases in position 'i'
-
+                gc[i]  += 1                      #counting GC content
+            totals[i] += 1 #regardless of what that base is, counting the total bases at position 'i'
     for i in range(len(gc)): #Now calculating average GC content
-        if totals[i] > 0: # So, we won't get error by dividing by zero
+        if totals[i] > 0: # So, we won't get an error when dividing by zero
             gc[i] /= float(totals[i]) #total CG count at that position/ Total bases at that position
     return gc
   
