3431670077215b536fd33ed8b9126c62bba43030
Jyoti Lama <97466232+JLama75@users.noreply.github.com>
2023-09-14 17:01:53 -0400

    Update Approximate_Matching.py

--- Algorithm_DNAsequence/Approximate_Matching.py
+++ Algorithm_DNAsequence/Approximate_Matching.py
@@ -1,4 +1,4 @@
-
+#Check BoyerMooreClass.py to get the code where I defined the class called BoyerMoore, which is used later on.
 def boyer_moore(p, p_bm, t):
     i = 0
     occurrences = [] #list to store indices that give matches
@@ -26,35 +26,45 @@ p = 'TCTA'
 p_bm = BoyerMoore(p) #calling function
 boyer_moore(p, p_bm, t)
 
-#Using approximate match algorithm using segmentation by applying Pigeonhole algorithm
-#segment the pattern by 1 + max no. of mismatches. Even in worst case senario one segment should has exact match to the text.
-#So perform exact matching (BoyerMoore) on all the segements generated from pattern 'p'.
+#Using approximate match algorithm using segmentation by applying the Pigeonhole algorithm.
+#Essentially we divide the pattern into segments (segment length = mismatches(n) + 1), such that worst case we still have one segment without any mismatches. 
+#We check each segment through the typical exact matching algorithm (Naive or Boyer-Moore method), which returns you the positions in the genome/text where the matching occurred.
+#Then we must align the rest of the segments that likely have the mismatches through character matching (matching each character of the remaining segments of the pattern with the corresponding character in the genome/text)
+#and count the number of mismatches so that only reporting those offsets/GenomicPositions where the mismatches are <= n.
+
+
+#segment the pattern by 1 + max no. of mismatches. Even in the worst-case scenario, one segment should have an exact match to the text.
+#So perform exact matching (BoyerMoore) on all the segments generated from pattern 'p'.
+
 def approximate_match(p, t, n): #n: max no. of mismatches
 #how many segments = no. of mismatches + 1
     segment_length = int(round(len(p)/ (n+1)))
-    all_matches = set() #would only store unique matches not duplicates
+    all_matches = set() #would only store unique matches, not duplicates
     for i in range(n+1):
         start = i*segment_length
-        end = min((i+1)* segment_length, len(p)) #resizing the last lenght of segment
+        end = min((i+1)* segment_length, len(p)) #resizing the last length of the segment since some segments would be short
+        #Passing the specific segment to BoyerMoore 
         p_bm = BoyerMoore(p[start:end], alphabet='ACGT')#makes the good character and bad suffix rule matrices
         matches = boyer_moore(p[start:end], p_bm, t)
+        #If perfect/exact match then it returns one or more positions/offsets in the genome where the start of the segment matched
 
+        #Now using each of those genomic positions/offsets (m), we will do character matching of the remaining segments of the pattern to the corresponding regions in the genome
         for m in matches:
-            if m < start or m+len(p)-start > len(t): #to make sure our pattern does not run off in the begininning or the end of t
+            if m < start or m+len(p)-start > len(t): #to make sure our pattern does not run off at the beginning or the end of t. (len(p) - start tells you the remaining characters to be matched)
                 continue
             mismatches = 0
-            for j in range(0, start):
-                if not p[j] == t[m-start+j]:
-                    mismatches += 1
+            for j in range(0, start): #Character matching the leftmost remaining segments
+                if not p[j] == t[m-start+j]: #(m-start) takes it to the position corresponding to the starting point of the pattern. + j allows for matching of the next corresponding character
+                    mismatches += 1 #counting mismatches
                     if mismatches > n:
                         break
 
-            for j in range(end, len(p)):
-                if not p[j] == t[m-start+j]:
-                     mismatches += 1
+            for j in range(end, len(p)): #Character matching the rightmost remaining segments
+                if not p[j] == t[m-start+j]: #(m-start) takes it to the position corresponding to the starting point of the pattern. + j allows for matching of the next corresponding character
+                     mismatches += 1 #counting mismatches
                      if mismatches > n:
                         break
-            if mismatches <= n:
+            if mismatches <= n: #reporting only those with mismatches <= n
                 all_matches.add(m - start)
     return list(all_matches)
 
